19018eb73e4a04d1163b390c2d80ae19ae591611
Trimex Mac 12 <trimexmac12@Trimexs-iMac-12.local>
2025-11-08 11:19:50 +0800

    4

---
+++ database/seeders/StudentTbleSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+
+class StudentTbleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 100; $i++) {
+            DB::table('student')->insert([
+                'firstname' => $faker->firstName,
+                'lastname' => $faker->lastName,
+                'email' => $faker->unique()->safeEmail,
+                'contact_no' => $faker->phoneNumber,
+                'password' => Hash::make('password123'), // Default password for all seeded users
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
